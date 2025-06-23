84923467121ef917676e2f3e5ab1554cd1336368
fh <thrustball007@gmail.com>
2025-06-23 20:14:27 +0200

    Fixes for form data to json

--- books/src/lib/newbook.svelte
+++ books/src/lib/newbook.svelte
@@ -1,81 +1,74 @@
 <form method="POST">
   <div class="form-group">
     <label for="title">Book Title</label>
-    <input type="text" class="form-control" id="title" aria-describedby="title" placeholder="Title">
+    <input type="text" class="form-control" id="title" name="title" placeholder="Title">
   </div>
   
   <br/> 
   
   <div class="form-group">
     <label for="author">Book Author</label>
-    <input type="text" class="form-control" id="author" aria-describedby="author" placeholder="Author">
+    <input type="text" class="form-control" id="author" name="author" placeholder="Author">
   </div>
 
   <br/>
 
   <div class="form-group">
     <label for="publishingdate">Publishing Date</label>
-    <input type="date" class="form-control" id="publishingdate" aria-describedby="publishingdate">
+    <input type="date" class="form-control" id="publishingdate" name="publishingdate">
   </div>
 
   <br/>
 
   <div class="form-group">
     <label for="purchaseddate">Purchased Date</label>
-    <input type="date" class="form-control" id="purchaseddate" aria-describedby="purchaseddate">
+    <input type="date" class="form-control" id="purchaseddate" name="purchaseddate">
   </div>
 
   <br/>
 
   <div class="form-group">
     <label for="publisher">Publisher</label>
-    <input type="text" class="form-control" id="publisher" aria-describedby="publisher" placeholder="Publisher">
+    <input type="text" class="form-control" id="publisher" name="publisher" placeholder="Publisher">
   </div>
 
   <br/>
 
   <div class="form-group">
     <label for="isbn">ISBN</label>
-    <input type="text" class="form-control" id="isbn" aria-describedby="isbn" placeholder="ISBN">
+    <input type="text" class="form-control" id="isbn" name="isbn" placeholder="ISBN">
   </div>
 
   <br/>
 
   <div class="form-group">
-    <label for="publishingdate">Publishing Date</label>
-    <input type="date" class="form-control" id="publishingdate" aria-describedby="publishingdate">
-  </div>
-
-  <br />
-
-  <div class="form-group">
     <label for="priceNewInput">Price (new)</label>
     <div class="input-group">
       <span class="input-group-text">€</span>
-      <input type="number" class="form-control" id="priceNewInput" placeholder="Enter price" min="0" step="0.01">
+      <input type="number" class="form-control" id="priceNewInput" name="price_new" placeholder="Enter price" min="0" step="0.01">
     </div>
   </div>
 
   <br />
 
   <div class="form-group">
-    <label for="priceboughtInput">Price (bought for)</label>
+    <label for="priceBoughtInput">Price (bought for)</label>
     <div class="input-group">
       <span class="input-group-text">€</span>
-      <input type="number" class="form-control" id="priceboughtInput" placeholder="Enter price" min="0" step="0.01">
+      <input type="number" class="form-control" id="priceBoughtInput" name="price_bought" placeholder="Enter price" min="0" step="0.01">
     </div>
   </div>
 
   <br />
 
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="newused" id="new">
+    <input class="form-check-input" type="radio" name="newused" id="new" value="new">
     <label class="form-check-label" for="new">
       New
     </label>
   </div>
   <div class="form-check">
-    <input class="form-check-input" type="radio" name="newused" id="used" checked>
+    <input class="form-check-input" type="radio" name="newused" id="used" value="used" checked>
     <label class="form-check-label" for="used">
       Used
     </label>
@@ -86,36 +79,39 @@
   <div class="form-group">
     <label for="pages">Pages</label>
     <div class="input-group">
-      <span class="input-group-text">€</span>
-      <input type="number" class="form-control" id="priceboughtInput" placeholder="Pages" min="0" step="1">
+      <span class="input-group-text">&#128214;</span>
+      <input type="number" class="form-control" id="pages" name="pages" placeholder="Pages" min="0" step="1">
     </div>
   </div>
 
   <br/>
 
-<select class="form-select" id="multiple-select-field" data-placeholder="Choose anything" multiple>
-    <option value="fantasy">Fantasy</option>
-    <option value="sci-fi">Science Fiction</option>
-    <option value="dystopian">Dystopian</option>
-    <option value="action-adventure">Action & Adventure</option>
-    <option value="mystery">Mystery</option>
-    <option value="horror">Horror</option>
-    <option value="thriller">Thriller & Suspense</option>
-    <option value="historical-fiction">Historical Fiction</option>
-    <option value="romance">Romance</option>
-    <option value="literary">Literary Fiction</option>
-    <option value="realistic">Realistic Fiction</option>
-    <option value="biography">Biography</option>
-    <option value="memoir">Memoir</option>
-    <option value="essay">Essay</option>
-    <option value="true-crime">True Crime</option>
-    <option value="travel">Travel</option>
-    <option value="history">History</option>
-    <option value="business">Business</option>
-    <option value="health-lifestyle">Health & Lifestyle</option>
-    <option value="food-drink">Food & Drink</option>
-    <option value="sports">Sports</option>
-  </select>
+  <div class="form-group">
+    <label for="genres">Genres</label>
+    <select class="form-select" id="genres" name="genres" multiple>
+      <option value="fantasy">Fantasy</option>
+      <option value="sci-fi">Science Fiction</option>
+      <option value="dystopian">Dystopian</option>
+      <option value="action-adventure">Action & Adventure</option>
+      <option value="mystery">Mystery</option>
+      <option value="horror">Horror</option>
+      <option value="thriller">Thriller & Suspense</option>
+      <option value="historical-fiction">Historical Fiction</option>
+      <option value="romance">Romance</option>
+      <option value="literary">Literary Fiction</option>
+      <option value="realistic">Realistic Fiction</option>
+      <option value="biography">Biography</option>
+      <option value="memoir">Memoir</option>
+      <option value="essay">Essay</option>
+      <option value="true-crime">True Crime</option>
+      <option value="travel">Travel</option>
+      <option value="history">History</option>
+      <option value="business">Business</option>
+      <option value="health-lifestyle">Health & Lifestyle</option>
+      <option value="food-drink">Food & Drink</option>
+      <option value="sports">Sports</option>
+    </select>
+  </div>
 
   <br/>
 
--- books/src/routes/addbook/+page.server.ts
+++ books/src/routes/addbook/+page.server.ts
@@ -3,7 +3,25 @@ import type { Actions } from './$types';
 export const actions = {
 	default: async ({ cookies, request }) => {
 		const data = await request.formData();
-        console.log(data);
+
+		const object: Record<string, unknown> = {};
+
+		data.forEach((value: unknown, key: string) => {
+			// Prefer Reflect.has over hasOwnProperty
+			if (!Reflect.has(object, key)) {
+				object[key] = value;
+				return;
+			}
+			if (!Array.isArray(object[key])) {
+				object[key] = [object[key]];
+			}
+			// Type assertion to tell TypeScript this is now an array
+			(object[key] as unknown[]).push(value);
+		});
+
+		const json = JSON.stringify(object);
+
+        console.log(json);
 		return { success: true };
 	}
 } satisfies Actions;
